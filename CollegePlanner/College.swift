bd53a45851e3d432cf86ca6fda3af60cfa28f4a4
student3 <student3@jh-mba158-25.d214.org>
2016-11-18 09:48:50 -0600

    test

---
+++ CollegePlanner/College.swift
@@ -0,0 +1,21 @@
+//
+//  College.swift
+//  CollegePlanner
+//
+//  Created by student3 on 11/18/16.
+//  Copyright © 2016 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class College{
+
+    var collegeName: String = ""
+    var studentbodyGender : String = ""
+    var collegeLocation : String = ""
+    var testing: String = ""
+    var action : String = ""
+    var essaysRequired: String = ""
+    
+    
+}
