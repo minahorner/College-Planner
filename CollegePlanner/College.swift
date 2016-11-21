7ab6ffa53c37c0fe980554aed4ba5114cbc8b01d
student3 <student3@jh-mba158-25.d214.org>
2016-11-21 09:33:06 -0600

    oiph

--- CollegePlanner/College.swift
+++ CollegePlanner/College.swift
@@ -12,9 +12,11 @@ class College{
     init() {
         
     }
+    
     init (name: String, mainGender: String, location: String, typeOfTest: String, decisionTime: String, essays: String, testPolicychoice: String){
         
     }
+    
     var collegeName: String = ""
     var studentbodyGender : String = ""
     var collegeLocation : String = ""
@@ -22,6 +24,5 @@ class College{
     var action : String = ""
     var essaysRequired: String = ""
     var testPolicy: String = ""
-    
     
 }
