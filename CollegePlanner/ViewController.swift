b9d46b850bab9d7d40926af2ebcbc5c385278f8c
student3 <student3@jh-mba158-08.d214.org>
2016-11-28 09:43:36 -0600

    editing stuff

--- CollegePlanner/ViewController.swift
+++ CollegePlanner/ViewController.swift
@@ -14,18 +14,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var collegeTableView: UITableView!
     
+    @IBOutlet weak var editButton: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
             collegeArray[0].collegeName = "Practice College"
         print(collegeArray[0].collegeName)
+        
         collegeTableView.delegate = self
         collegeTableView.dataSource = self
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        self.collegeTableView.setEditing(editing, animated: animated)
+    }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return collegeArray.count
@@ -36,6 +46,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = collegeArray[indexPath.row].collegeName
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedCollege = collegeArray[sourceIndexPath.row]
+        collegeArray.remove(at: sourceIndexPath.row)
+        collegeArray.insert(movedCollege, at: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            print("doing")
+            collegeArray.remove(at: indexPath.row)
+            collegeTableView.reloadData()
+        }
+    }
+
 
 }
 
