c557e6121d5cba156b762c235e4f58fa66dafe46
student3 <student3@jh-mba158-08.d214.org>
2017-02-27 09:59:05 -0600

    scsdfafadsd

---
+++ CollegePlanner/Protocol.swift
@@ -0,0 +1,15 @@
+//
+//  Protocol.swift
+//  CollegePlanner
+//
+//  Created by student3 on 2/3/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+protocol sendBackDateDelegate{
+    
+    func sendDate(date: String)
+
+}
