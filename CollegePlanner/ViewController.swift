9564ef72fc2c786a97a6b9aeb1e6c80341f83a58
Mary <yxu7400@stu.d214.org>
2017-04-17 09:44:12 -0500

    awe

--- CollegePlanner/ViewController.swift
+++ CollegePlanner/ViewController.swift
@@ -44,123 +44,127 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.navigationController?.navigationBar.barStyle = UIBarStyle.black
         self.navigationController?.navigationBar.tintColor = UIColor.white
         
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    //sends to viewcontrollers
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if(segue.identifier == "segueToAddCollege"){
-            print("yep")
-            let vc = segue.destination as! AddCollegeViewController
-            vc.previousTableView = self.collegeTableView
-            vc.previousVC = self
-            vc.database = self.database
-        }
-        if(segue.identifier == "segueToDetailed"){
-            var num = (collegeTableView.indexPathForSelectedRow?.row)!
-            let vc = segue.destination as! collegeDetailedViewController
-            vc.collegeArray = self.collegeArray
-            vc.chosen = (collegeTableView.indexPathForSelectedRow?.row)!
-            vc.previousVC = self
-        }
-    }
-    
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        self.collegeTableView.setEditing(editing, animated: animated)
-        if(editing){
-            add.isEnabled = false
-        }
-        else{
-            add.isEnabled = true
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
-        return collegeArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = collegeTableView.dequeueReusableCell(withIdentifier: "collegeCell", for: indexPath)
-        cell.textLabel?.text = collegeArray[(indexPath as NSIndexPath).row].collegeName
-        return cell
     }
     
     
-    //func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-    //    let movedCollege = collegeArray[sourceIndexPath.row]
-    //    collegeArray.remove(at: sourceIndexPath.row)
-    //    collegeArray.insert(movedCollege, at: destinationIndexPath.row)
-    //add save function to save order of college
-    //}
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if (editingStyle == .delete) {
-            print("doing")
-            let record = CKRecordID(recordName: collegeArray[indexPath.row].recordID)
-            database.delete(withRecordID: record) { (ID, error) in
-                if error == nil{
-                    print("Deleted")
-                }
-                else{
-                    print(error)
-                }
+        //sends to viewcontrollers
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if(segue.identifier == "segueToAddCollege"){
+                print("yep")
+                let vc = segue.destination as! AddCollegeViewController
+                vc.previousTableView = self.collegeTableView
+                vc.previousVC = self
+                vc.database = self.database
             }
-            self.collegeArray.remove(at: indexPath.row)
-            collegeTableView.reloadData()
+            if(segue.identifier == "segueToDetailed"){
+                var num = (collegeTableView.indexPathForSelectedRow?.row)!
+                let vc = segue.destination as! collegeDetailedViewController
+                vc.collegeArray = self.collegeArray
+                vc.chosen = (collegeTableView.indexPathForSelectedRow?.row)!
+                vc.previousVC = self
+            }
         }
-    }
     
-    func getColleges(){
-        
-        collegeArray = []
-        let predicate = NSPredicate(value: true)
-        let query = CKQuery(recordType: "College", predicate: predicate)
-        
-        database.perform(query, inZoneWith: nil) { (records, error) in
-            
-            for college in records!{
-                let name = college.object(forKey: "name") as! String
-                let location = college.object(forKey: "location") as! String
-                let testType = college.object(forKey: "test") as! String
-                let decisionDate = college.object(forKey: "decisionDate") as! String
-                let essaysRequired = college.object(forKey: "essaysRequired") as! String
-                let counselorRecNeeded = college.object(forKey: "counselorRec") as! String
-                let teacherRecNeeded = college.object(forKey: "teacherRec") as! String
-                let login = college.object(forKey: "login") as! String
-                let password = college.object(forKey: "password") as! String
-                let difficulty = college.object(forKey: "difficulty") as! String
-                let teacherRecDone = college.object(forKey: "teacherRecDone") as! String
-                let counselorRecDone = college.object(forKey: "counselorRecDone") as! String
-                let essaysDone = college.object(forKey: "essaysDone") as! String
-                let testSent = college.object(forKey: "testSent") as! String
-                let collegeType = college.object(forKey: "collegeType") as! String
-                //                let allCollegeOrder = college.object(forKey: "allCollegeOrder") as! Int64
-                //                let thinkingAboutCollegeOrder = college.object(forKey: "thinkingAboutCollegesOrder") as! Int64
-                //                let appliedToCollegeOrder = college.object(forKey: "applyingToCollegesOrder") as! Int64
-                //                let acceptedCollegeOrder = college.object(forKey: "acceptedCollegesOrder") as! Int64
-                let currentCollege = College(collegeName: name, collegeLocation: location, testType: testType, decisionDate: decisionDate, essaysRequired: essaysRequired, login: login, password: password, difficulty: difficulty, counselorRecNeeded: counselorRecNeeded, counselorRecDone: counselorRecDone, teacherRecNeeded: teacherRecNeeded, teacherRecDone: teacherRecDone, essaysDone: essaysDone, testSent: testSent, collegeType : collegeType, allCollegeOrder : 0, thinkingAboutCollegeOrder : 0, appliedToCollegeOrder : 0, acceptedCollegeOrder : 0, RecordID : college.recordID)
-                
-                
-                
-                if (self.collegeTypePage == 0 || (self.collegeTypePage == 1 && currentCollege.collegeType == "1") || (self.collegeTypePage == 2 && currentCollege.collegeType == "2") || (self.collegeTypePage == 3 && currentCollege.collegeType == "3")){
+        override func setEditing(_ editing: Bool, animated: Bool) {
+            super.setEditing(editing, animated: animated)
+            self.collegeTableView.setEditing(editing, animated: animated)
+            if(editing){
+                add.isEnabled = false
+            }
+            else{
+                add.isEnabled = true
+            }
+        }
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    
+            return collegeArray.count
+        }
+    
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+            let cell = collegeTableView.dequeueReusableCell(withIdentifier: "collegeCell", for: indexPath)
+            cell.textLabel?.text = collegeArray[(indexPath as NSIndexPath).row].collegeName
+            return cell
+        }
+    
+    
+        //func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //    let movedCollege = collegeArray[sourceIndexPath.row]
+        //    collegeArray.remove(at: sourceIndexPath.row)
+        //    collegeArray.insert(movedCollege, at: destinationIndexPath.row)
+        //add save function to save order of college
+        //}
+    
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+            if (editingStyle == .delete) {
+                print("doing")
+                let record = CKRecordID(recordName: collegeArray[indexPath.row].recordID)
+                database.delete(withRecordID: record) { (ID, error) in
+                    if error == nil{
+                        print("Deleted")
+                    }
+                    else{
+                        print(error)
+                    }
+                }
+                self.collegeArray.remove(at: indexPath.row)
+                collegeTableView.reloadData()
+            }
+        }
+    
+        func getColleges(){
+    
+            collegeArray = []
+            let predicate = NSPredicate(value: true)
+            let query = CKQuery(recordType: "College", predicate: predicate)
+    
+            database.perform(query, inZoneWith: nil) { (records, error) in
+    
+                for college in records!{
+                    let name = college.object(forKey: "name") as! String
+                    let location = college.object(forKey: "location") as! String
+                    let testType = college.object(forKey: "test") as! String
+                    let decisionDate = college.object(forKey: "decisionDate") as! String
+                    let essaysRequired = college.object(forKey: "essaysRequired") as! String
+                    let counselorRecNeeded = college.object(forKey: "counselorRec") as! String
+                    let teacherRecNeeded = college.object(forKey: "teacherRec") as! String
+                    let login = college.object(forKey: "login") as! String
+                    let password = college.object(forKey: "password") as! String
+                    let difficulty = college.object(forKey: "difficulty") as! String
+                    let teacherRecDone = college.object(forKey: "teacherRecDone") as! String
+                    let counselorRecDone = college.object(forKey: "counselorRecDone") as! String
+                    let essaysDone = college.object(forKey: "essaysDone") as! String
+                    let testSent = college.object(forKey: "testSent") as! String
+                    let collegeType = college.object(forKey: "collegeType") as! String
+                    let collegeRecord = college.object(forKey: "collegeID") as! String
+                    //                let allCollegeOrder = college.object(forKey: "allCollegeOrder") as! Int64
+                    //                let thinkingAboutCollegeOrder = college.object(forKey: "thinkingAboutCollegesOrder") as! Int64
+                    //                let appliedToCollegeOrder = college.object(forKey: "applyingToCollegesOrder") as! Int64
+                    //                let acceptedCollegeOrder = college.object(forKey: "acceptedCollegesOrder") as! Int64
+                    let currentCollege = College(collegeName: name, collegeLocation: location, testType: testType, decisionDate: decisionDate, essaysRequired: essaysRequired, login: login, password: password, difficulty: difficulty, counselorRecNeeded: counselorRecNeeded, counselorRecDone: counselorRecDone, teacherRecNeeded: teacherRecNeeded, teacherRecDone: teacherRecDone, essaysDone: essaysDone, testSent: testSent, collegeType : collegeType, allCollegeOrder : 0, thinkingAboutCollegeOrder : 0, appliedToCollegeOrder : 0, acceptedCollegeOrder : 0, RecordID : collegeRecord)
+    
+    
+    
+    
+    
+                    if (self.collegeTypePage == 0 || (self.collegeTypePage == 1 && currentCollege.collegeType == "1") || (self.collegeTypePage == 2 && currentCollege.collegeType == "2") || (self.collegeTypePage == 3 && currentCollege.collegeType == "3")){
+                        
+                        self.collegeArray.append(currentCollege)
+                    }
                     
-                    self.collegeArray.append(currentCollege)
+                    
+                    
+                }
+                DispatchQueue.main.async {
+                    self.collegeTableView.reloadData()
                 }
                 
-                
-                
             }
-            DispatchQueue.main.async {
-                self.collegeTableView.reloadData()
-            }
-            
         }
-    }
 }
